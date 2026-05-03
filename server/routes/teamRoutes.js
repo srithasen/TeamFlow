5a0fbe41e4cb8aae734ad550d58f3889771b9b64
srithasen <srithasen05@gmail.com>
2026-05-03 20:23:17 +0530

    Fixed authorizeRoles import

--- server/routes/teamRoutes.js
+++ server/routes/teamRoutes.js
@@ -5,7 +5,10 @@ const router = express.Router();
 const db = require("../config/db");
 
 const authMiddleware =
-  require("../middleware/authMiddleware");
+require("../middleware/authMiddleware");
+
+const authorizeRoles =
+require("../middleware/roleMiddleware");
 
 router.get(
   "/",
@@ -42,72 +45,6 @@ router.get(
 
   }
 
-);
-
-router.post(
-  "/",
-
-  authMiddleware,
-
-  authorizeRoles("admin"),
-
-  (req, res) => {
-
-    const {
-      project_id,
-      assigned_to,
-      title,
-      description,
-      deadline
-    } = req.body;
-
-    const sql = `
-      INSERT INTO tasks
-      (
-        project_id,
-        assigned_to,
-        title,
-        description,
-        status,
-        deadline
-      )
-      VALUES (?, ?, ?, ?, ?, ?)
-    `;
-
-    db.query(
-
-      sql,
-
-      [
-        project_id,
-        assigned_to,
-        title,
-        description,
-        "pending",
-        deadline
-      ],
-
-      (err, result) => {
-
-        if (err) {
-
-          console.log(err);
-
-          return res.status(500).json({
-            message: "Database error"
-          });
-
-        }
-
-        res.status(201).json({
-          message: "Task created"
-        });
-
-      }
-
-    );
-
-  }
 );
 
 module.exports = router;
