b9b97ea90089613a0234d49a6a2f59cffde39a19
srithasen <srithasen05@gmail.com>
2026-05-03 22:14:34 +0530

    Fixed Vite allowed hosts

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+
+  server: {
+
+    allowedHosts: [
+      "teamflow-production-e26e.up.railway.app"
+    ]
+
+  }
+
+});
