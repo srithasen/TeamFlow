63bd8672e998fd725a047df581f0453d08cb7627
srithasen <srithasen05@gmail.com>
2026-05-03 23:42:42 +0530

    Converted auth controller to PostgreSQL

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
     connectionString:
-        "postgresql://postgres:[Srithasen@123]@db.aovjfdygivecjlubhvmh.supabase.co:5432/postgres",
+        "postgresql://postgres:Srithasen@123@db.aovjfdygivecjlubhvmh.supabase.co:5432/postgres",
     ssl: {
         rejectUnauthorized: false,
     },
